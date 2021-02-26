06232230591fcea6b4d91cdc51596620db06c0ee
Vaqim <vadimser1355@gmail.com>
2021-02-26 13:25:19 +0200

    views added

--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,20 @@ const contactsRoute = require('./routes/contacts');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './src/server/views/pages');
+
+app.use('/', express.static('./src/server/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from home page!' });
+  res.send('Home page!');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
 app.use('/contacts', contactsRoute);
