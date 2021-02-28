1c12b9616a6c5531853a50fb4e9ccf72de1fc0ab
Vaqim <vadimser1355@gmail.com>
2021-02-28 16:03:53 +0200

    delete odd console

--- src/server/middlewares/checkAuthorization.js
+++ src/server/middlewares/checkAuthorization.js
@@ -3,7 +3,6 @@ const {
 } = require('../../config');
 
 module.exports = (req, res, next) => {
-  console.log(req.session.user, userData.username);
   if (req.session.user === userData.username) next();
   else res.render('login', { errorMessage: 'Authorization required!' });
 };
